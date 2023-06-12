5142426e9d5dabe825e8e2e62b20c16493d981f0
Petr Legen <p.legen@post.cz>
2023-06-12 10:29:00 +0200

    Napojení databáze postgres

--- Evidence_pojistnych_udalosti/settings.py
+++ Evidence_pojistnych_udalosti/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Evidence_pojistnych_udalosti.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Evidence_pojistnych_udalosti',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': '*',
+        'PORT': '5432',
     }
 }
 
