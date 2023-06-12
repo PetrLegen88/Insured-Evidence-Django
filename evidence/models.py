fc986d5d21fd6116d7c77d9d0c2fd65a8200c521
Petr Legen <p.legen@post.cz>
2023-06-12 13:11:40 +0200

    přidání aplikace evidence do settings.py vytvoření models.py (Insured,Insurance,InsuranceEvent)

--- evidence/models.py
+++ evidence/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Insured(models.Model):
+    ROLE_CHOICES = (
+        ('insurer', 'Insurer'),
+        ('insured', 'Insured'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=64)
+    zipcode = models.IntegerField()
+    phone = models.CharField(max_length=16)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Insurance(models.Model):
+    INSURANCE_TYPE = (
+        ('home', 'Home insurance'),
+        ('car', 'Car insurance'),
+        ('property', 'Property insurance'),
+
+    )
+    insurance = models.ForeignKey(Insured, on_delete=models.CASCADE)
+    type = models.CharField(max_length=32, choices=INSURANCE_TYPE)
+    subject = models.CharField(max_length=32)
+    amount = models.IntegerField()
+    valid_from = models.DateTimeField()
+    valid_until = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.insurance} {self.amount} {self.subject} {self.valid_from} {self.valid_until}'
+
+
+class InsuranceEvent(models.Model):
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('approved', 'Approved'),
+        ('rejected', 'Rejected'),
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed'),
+    )
+    insurance = models.ForeignKey(Insurance, on_delete=models.CASCADE)
+    description = models.TextField()
+    date = models.DateField()
+    status = models.CharField(max_length=32, choices=STATUS_CHOICES)
+    note = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f'Insurance event: {self.description}'
