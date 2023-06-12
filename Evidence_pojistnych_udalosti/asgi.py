a18cbc3054cb9385928bdbbf02cf2f185ebbcadb
Petr Legen <p.legen@post.cz>
2023-06-12 09:56:34 +0200

    Vytvoření projektu Evidence_pojistnych_udalosti Vytvoření aplikace evidence

---
+++ Evidence_pojistnych_udalosti/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Evidence_pojistnych_udalosti project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Evidence_pojistnych_udalosti.settings')
+
+application = get_asgi_application()
